53430e9ad147175e0e4253f6913b88c427875592
Morgan <heshammse@gmail.com>
2018-02-21 17:18:18 -0800

    added a file

---
+++ nodejs-seed-project-es5/runOnce.js
@@ -0,0 +1,19 @@
+
+const mongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+
+mongoClient.connect('mongodb://localhost:27017',function(err,client){
+
+   assert.equal(null,err);
+   const db=client.db("myproject");
+   console.log("Connected!!");
+  // db.collection("users").insert({"name":"Morgan","password":"Hello"}, function(err,result){
+    // assert.equal(null,err);
+    // console.log(result);
+  // });
+   db.collection("users").find({"name":"Morgan"}).toArray(function(err,result){
+     assert.equal(null,err);
+     console.log(result);
+   });
+});
