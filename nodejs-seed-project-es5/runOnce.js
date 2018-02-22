0c7f061c9f083fc6203e2062df60fd5a3ad1c294
Morgan <heshammse@gmail.com>
2018-02-22 09:52:31 -0800

    Finished a frontend Example for cards

--- nodejs-seed-project-es5/runOnce.js
+++ nodejs-seed-project-es5/runOnce.js
@@ -8,10 +8,10 @@ mongoClient.connect('mongodb://localhost:27017',function(err,client){
    assert.equal(null,err);
    const db=client.db("myproject");
    console.log("Connected!!");
-  // db.collection("users").insert({"name":"Morgan","password":"Hello"}, function(err,result){
-    // assert.equal(null,err);
-    // console.log(result);
-  // });
+   db.collection("users").insert({"name":"Morgan","password":"Hello"}, function(err,result){
+     assert.equal(null,err);
+    console.log(result);
+   });
    db.collection("users").find({"name":"Morgan"}).toArray(function(err,result){
      assert.equal(null,err);
      console.log(result);
