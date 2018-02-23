2746439e05c39ae54dca4f99d48695035d671012
ghostv247 <34899032+ghostv247@users.noreply.github.com>
2018-02-23 10:44:37 +0200

    Login Page Update , Nodejs update , Added MongoDB and Chrome page fix

---
+++ angular/src/app/@theme/layouts/index.ts
@@ -0,0 +1,2 @@
+export * from './main/main.layout';
+export * from './login/login.layout';
---
+++ backend-nodejs/api/models/User.js
@@ -0,0 +1,16 @@
+
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+},{collection: 'Users'}
+);
+
+mongoose.model('User', userSchema);
