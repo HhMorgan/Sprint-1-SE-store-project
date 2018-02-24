561f3e54e7d565b0f7f0ac015cadda0279b5c1a1
Mahmoud Gamal <mahmoud.gamal1595@gmail.com>
2018-02-25 00:06:28 +0200

    Message : added  5 pictures and descriptions for people

--- angular/src/app/@core/data/users.service.ts
+++ angular/src/app/@core/data/users.service.ts
@@ -8,11 +8,11 @@ let counter = 0;
 export class UserService {
 
   private users = {
-    AhmedAshraf: { name: 'Ahmed Ashraf', picture: 'assets/images/ahmedashraf.jpg',Description : '???'},
-    AhmedAyman: { name: 'Ahmed Ayman', picture: 'assets/images/Ahmedayman.jpg',Description : '???' },
+    AhmedAshraf: { name: 'Ahmed Ashraf', picture: 'assets/images/ahmedashraf.jpg', Description : '???'},
+    AhmedAyman: { name: 'Ahmed Ayman', picture: 'assets/images/Ahmedayman.jpg', Description : '???' },
     Aya2: { name: 'Aya', picture: 'assets/images/aya2.jpg' ,Description : '???'},
-    MahmmoudGamal: { name: 'Mahmmoud Gamal', picture: 'assets/images/MahmmoudGamal.jpg' ,Description : 'I love chess.'},
-    Habiba: { name: 'Habiba', picture: 'assets/images/HabibaElHussein.jpg',Description : 'I love cooking' },
+    MahmmoudGamal: { name: 'Mahmmoud Gamal', picture: 'assets/images/MahmmoudGamal.jpg' , Description : 'I love chess.'},
+    Habiba: { name: 'Habiba', picture: 'assets/images/HabibaElHussein.jpg', Description : 'I love cooking' },
     // eva: { name: 'Eva Moor', picture: 'assets/images/eva.png' },
     // jack: { name: 'Jack Williams', picture: 'assets/images/jack.png' },
     // lee: { name: 'Lee Wong', picture: 'assets/images/lee.png' },
