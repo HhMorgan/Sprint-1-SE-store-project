a0e57d509722625c48bc10f8f1fb0eb5e118a793
Morgan <heshammse@gmail.com>
2018-02-24 14:22:55 -0800

    big fixed

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -3,14 +3,7 @@ import { NgModule } from '@angular/core';
 
 const routes: Routes = [
   { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' },
-  {
-    path: 'login',
-    loadChildren: './login/login.module#LoginModule'
-  },
-  {
-    path: 'signup',
-    loadChildren: './signup/signup.module#SignupModule'
-  },
+   
   { path: '', redirectTo: 'pages', pathMatch: 'full' },
   { path: '**', redirectTo: 'pages' },
 ];
--- angular/src/app/pages/login/login-routing.module.ts
+++ angular/src/app/pages/login/login-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './login.component';
+import { LoginComponent } from '../login/login.component';
 
 const routes: Routes = [
   {
