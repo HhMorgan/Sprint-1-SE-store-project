963db6fc2e2de71337b573baf6cd7c6ef0ea814a
Morgan <heshammse@gmail.com>
2018-02-24 11:09:08 -0800

    Switched Seed

--- angular-seed-project/src/app/@theme/layouts/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './main/main.layout';
-export * from './login/login.layout';
--- angular-seed-project/src/app/dashboard/items/items.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-dashboard-items',
-  template: ``
-})
-export class ItemsComponent {}
