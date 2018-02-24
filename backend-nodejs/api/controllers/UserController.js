b8bd92689868475e3a45a42f50d0889ea3232187
ghostv247 <34899032+ghostv247@users.noreply.github.com>
2018-02-24 22:54:00 +0200

    fixed login issue

--- backend-nodejs/api/controllers/UserController.js
+++ backend-nodejs/api/controllers/UserController.js
@@ -7,7 +7,7 @@ User = mongoose.model('User');
 module.exports.userlogin = function(req, res, next) {
     var valid =  req.body.username && Validations.isString(req.body.username) &&
       req.body.password && Validations.isString(req.body.password);
-      // console.log(req.body);
+      console.log(req.body);
     if (!valid) {
       return res.status(422).json({
         err: null,
@@ -15,7 +15,7 @@ module.exports.userlogin = function(req, res, next) {
         data: null
       });
     }
-    User.findOne({ user:{ $eq: req.body.user } , password:{ $eq: req.body.password } }, function(err, user){
+    User.findOne({ username:{ $eq: req.body.username } , password:{ $eq: req.body.password } }, function(err, user){
       if (err) 
         throw err;
       if (user == null){
