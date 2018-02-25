32abe5f2792122345160f83c51160ce7d7347a1d
Tarek Abdelrahman <tarek.abdelghaffar@student.guc.edu.eg>
2018-02-25 19:16:35 +0200

    Adding Images and Info

--- angular/src/app/@core/data/users.service.ts
+++ angular/src/app/@core/data/users.service.ts
@@ -8,13 +8,15 @@ let counter = 0;
 export class UserService {
 
   private users = {
-    AhmedAshraf: { name: 'Ahmed Ashraf', picture: 'assets/images/ahmedashraf.jpg', Description : '???'},
-    AhmedAyman: { name: 'Ahmed Ayman', picture: 'assets/images/Ahmedayman.jpg', Description : '???' },
+    AhmedAshraf: { name: 'Ahmed Ashraf', picture: 'assets/images/ahmedashraf.jpg'
+    , Description : '???'},
+    AhmedAyman: { name: 'Ahmed Ayman', picture: 'assets/images/Ahmedayman.jpg',
+    Description : '???'},
     Aya2: { name: 'Aya', picture: 'assets/images/aya2.jpg' , Description : '???'},
     MahmmoudGamal: { name: 'Mahmmoud Gamal', picture: 'assets/images/MahmmoudGamal.jpg'
     , Description : 'I love chess.'},
     Habiba: { name: 'Habiba', picture: 'assets/images/HabibaElHussein.jpg'
-    , Description : 'I love cooking' },
+    , Description : 'I love cooking'},
     MayarLotfy: { name: 'MayarLotfy', picture: 'assets/images/MayarLotfy.jpg',
     Description : 'I am a passionate Archery player' },
     MohamedAdel: { name: 'MohamedAdel', picture: 'assets/images/MohamedAdel.JPG',
@@ -23,15 +25,15 @@ export class UserService {
     Description : 'I love Tech' },
     Nada: { name: 'Nada', picture: 'assets/images/nada.jpg', Description : '???'},
     Sarah :{ name:'Sara Gamal', picture:'assets/images/SarahGamal.jpg',
-    Description:'Languages, impressionism and symbols.' },
+    Description:'Languages, impressionism and symbols.'},
     Tarek :{ name:'Tarek Abdelrahman', picture:'assets/images/Tarek.JPG',
     Description:'Football is my passion' },
-    Youhanna :{ name:'Youhanna Adel', picture:'assets/images/youhannaadel.jpg',
-    Description:'???' },
-    Mariam :{ name:'Mariam Salama', picture:'assets/images/Mariam.JPG', Description:'I like to read' },
-    Salma :{ name:'Salma Zaki', picture:'assets/images/salmazaki.jpg', Description:'???' },
-    Shaker :{ name:'Omar Shaker', picture:'assets/images/OmarShaker.jpg', Description:'I love coding.' },
-    Hesham :{ name:'Mohamed Hesham', picture:'assets/images/mohamed_hesham.jpg', Description:'I love drawing and art in general' },
+    Youhanna :{ name:'Youhanna Adel',picture:'assets/images/youhannaadel.jpg',
+    Description:'???'},
+    Mariam :{ name:'Mariam Salama', picture:'assets/images/Mariam.JPG', Description:'I like to read'},
+    Salma :{ name:'Salma Zaki', picture:'assets/images/salmazaki.jpg', Description:'???'},
+    Shaker :{ name:'Omar Shaker', picture:'assets/images/OmarShaker.jpg', Description:'I love coding.'},
+    Hesham :{ name:'Mohamed Hesham', picture:'assets/images/mohamed_hesham.jpg', Description:'I love drawing and art in general'},
 
   };
 
