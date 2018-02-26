116733470cd191a2ae8d1250639ad4b98643bd15
ghostv247 <34899032+ghostv247@users.noreply.github.com>
2018-02-26 17:00:46 +0200

    added RunMeOnce.js

--- angular/src/app/pages/login/login.component.ts
+++ angular/src/app/pages/login/login.component.ts
@@ -26,6 +26,7 @@ export class LoginComponent implements OnInit {
 
   loginClick(){
     this._apiService.login({ username: this.username, password: this.password }).subscribe((apiresponse: APIData)=>{
+      this.loginMessage = apiresponse.msg;
       if( apiresponse.msg.includes('Successful') ){ //D2a 7aga mo2kta
         this.showDashboard();
       } else {
