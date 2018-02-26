c068a2c4b8d08db7013d474a9041e7d5568005a8
Deathyz <mohamed.medou@gmail.com>
2018-02-26 12:14:03 +0200

    Message: Integrated manually the pictures added and integrated the new user Object

--- angular/src/app/pages/dashboard/contacts/contacts.component.ts
+++ angular/src/app/pages/dashboard/contacts/contacts.component.ts
@@ -32,26 +32,60 @@ export class ContactsComponent implements OnInit, OnDestroy {
     this.userService.getUsers()
       .subscribe((users: any) => {
         this.contacts = [
-          {user: users.hesham, type: 'mobile'},
-          {user: users.eva, type: 'home'},
-          {user: users.jack, type: 'mobile'},
-          {user: users.lee, type: 'mobile'},
-          {user: users.alan, type: 'home'},
-          {user: users.kate, type: 'work'},
+//          {user: users.nick, type: 'mobile'},
+          // {user: users.eva, type: 'home'},
+          // {user: users.jack, type: 'mobile'},
+          // {user: users.lee, type: 'mobile'},
+          // {user: users.alan, type: 'home'},
+          // {user: users.kate, type: 'work'},
+        {user: users.AhmedAshraf},
+        {user: users.AhmedAyman},
+        {user: users.Aya2},
+        {user: users.Habiba},
+        {user: users.MahmmoudGamal},
+        {user: users.MayarLotfy},
+        {user: users.MohamedAdel},
+        {user: users.Morgan},
+        {user: users.Nada},
+        {user: users.Sarah},
+        {user: users.Tarek},
+        {user: users.Youhanna},
+        {user: users.Mariam},
+        {user: users.Salma},
+        {user: users.Shaker},
+        {user: users.Hesham},
+
+
+
         ];
 
         this.recent = [
-          {user: users.alan, type: 'home', time: '9:12 pm'},
-          {user: users.eva, type: 'home', time: '7:45 pm'},
-          {user: users.hesham, type: 'mobile', time: '5:29 pm'},
-          {user: users.lee, type: 'mobile', time: '11:24 am'},
-          {user: users.jack, type: 'mobile', time: '10:45 am'},
-          {user: users.kate, type: 'work', time: '9:42 am'},
-          {user: users.kate, type: 'work', time: '9:31 am'},
-          {user: users.jack, type: 'mobile', time: '8:01 am'},
-        ];
-      });
-  }
+          // {user: users.alan, type: 'home', time: '9:12 pm'},
+          // {user: users.eva, type: 'home', time: '7:45 pm'},
+          {user: users.AhmedAshraf},
+          {user: users.AhmedAyman},
+          {user: users.Aya2},
+          {user: users.Habiba},
+          {user: users.MahmmoudGamal},
+          {user: users.MayarLotfy},
+          {user: users.MohamedAdel},
+          {user: users.Morgan},
+          {user: users.Nada},
+          {user: users.Sarah},
+          {user: users.Tarek},
+          {user: users.Youhanna},
+          {user: users.Mariam},
+          {user: users.Salma},
+          {user: users.Shaker},
+          {user: users.Hesham},
+
+
+          // {user: users.lee, type: 'mobile', time: '11:24 am'},
+          // {user: users.jack, type: 'mobile', time: '10:45 am'},
+          // {user: users.kate, type: 'work', time: '9:42 am'},
+          // {user: users.kate, type: 'work', time: '9:31 am'},
+          // {user: users.jack, type: 'mobile', time: '8:01 am'},
+   ]})}
 
   ngOnDestroy() {
     this.themeSubscription.unsubscribe();
