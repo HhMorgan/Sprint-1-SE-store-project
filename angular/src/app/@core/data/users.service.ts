ee9cb8a443a8e3a1777b3941e11a5a91a52a8a90
Deathyz <mohamed.medou@gmail.com>
2018-02-26 12:14:03 +0200

    Message: Integrated manually the pictures added and integrated the new user Object

--- angular/src/app/@core/data/users.service.ts
+++ angular/src/app/@core/data/users.service.ts
@@ -8,12 +8,33 @@ let counter = 0;
 export class UserService {
 
   private users = {
-    hesham: { name: 'Hesham Morgan', picture: 'assets/images/nick.png' },
-    eva: { name: 'Eva Moor', picture: 'assets/images/eva.png' },
-    jack: { name: 'Jack Williams', picture: 'assets/images/jack.png' },
-    lee: { name: 'Lee Wong', picture: 'assets/images/lee.png' },
-    alan: { name: 'Alan Thompson', picture: 'assets/images/alan.png' },
-    kate: { name: 'Kate Martinez', picture: 'assets/images/kate.png' },
+    AhmedAshraf: { name: 'Ahmed Ashraf', picture: 'assets/images/ahmedashraf.jpg'
+    , Description : '???'},
+    AhmedAyman: { name: 'Ahmed Ayman', picture: 'assets/images/Ahmedayman.jpg',
+    Description : '???'},
+    Aya2: { name: 'Aya', picture: 'assets/images/aya2.jpg' , Description : '???'},
+    MahmmoudGamal: { name: 'Mahmmoud Gamal', picture: 'assets/images/MahmoudGamal.jpg'
+    , Description : 'I love chess.'},
+    Habiba: { name: 'Habiba', picture: 'assets/images/HabibaElHussein.jpg'
+    , Description : 'I love cooking'},
+    MayarLotfy: { name: 'MayarLotfy', picture: 'assets/images/MayarLotfy.jpg',
+    Description : 'I am a passionate Archery player' },
+    MohamedAdel: { name: 'MohamedAdel', picture: 'assets/images/MohamedAdel.JPG',
+    Description : 'I love Debugging' },
+    Morgan: { name: 'Hesham Morgan', picture: 'assets/images/morgan.jpg',
+    Description : 'I love Tech' },
+    Nada: { name: 'Nada', picture: 'assets/images/nada.jpg', Description : '???'},
+    Sarah :{ name:'Sara Gamal', picture:'assets/images/SarahGamal.jpg',
+    Description:'Languages, impressionism and symbols.'},
+    Tarek :{ name:'Tarek Abdelrahman', picture:'assets/images/Tarek.JPG',
+    Description:'Football is my passion' },
+    Youhanna :{ name:'Youhanna Adel',picture:'assets/images/youhannaadel.jpg',
+    Description:'???'},
+    Mariam :{ name:'Mariam Salama', picture:'assets/images/Mariam.JPG', Description:'I like to read'},
+    Salma :{ name:'Salma Zaki', picture:'assets/images/salmazaki.jpg', Description:'???'},
+    Shaker :{ name:'Omar Shaker', picture:'assets/images/OmarShaker.jpg', Description:'I love coding.'},
+    Hesham :{ name:'Mohamed Hesham', picture:'assets/images/mohamed_hesham.jpg', Description:'I love drawing and art in general'},
+
   };
 
   private userArray: any[];
