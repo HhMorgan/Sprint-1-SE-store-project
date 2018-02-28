61eca7664bab9f7cb4d02fa16e590cb2aebcab4f
Nour Nounou <nournounou@hotmail.co.uk>
2018-02-28 13:43:00 +0200

    Added the pictures and descriptions of people and myself.

--- angular/src/app/@core/data/users.service.ts
+++ angular/src/app/@core/data/users.service.ts
@@ -10,9 +10,19 @@ export class UserService {
   private users = {
     AhmedAshraf: { name: 'Ahmed Ashraf', picture: 'assets/images/ahmedashraf.jpg'
     , Description : '???'},
+  OmarHany: { name: 'Omar Hany', picture: 'assets/images/OmarHany.jpg'
+    , Description : '???'},
+    NourNounou: { name: 'Nour Nounou', picture: 'assets/images/NourNounou.JPG'
+    , Description : 'I love SE'},
+    EbramIbrahim: { name: 'Ebram Ibrahim', picture: 'assets/images/EbramIbrahim.jpg'
+    , Description : 'I love reading'},
+    AhmedAlaa: { name: 'Ahmed Alaa', picture: 'assets/images/AhmedAlaa.jpg'
+    , Description : 'I love diving'},
+    AhmedHany: { name: 'Ahmed Hany', picture: 'assets/images/AhmedHany.jpg',
+    Description : 'I am a writer'},
     AhmedAyman: { name: 'Ahmed Ayman', picture: 'assets/images/Ahmedayman.jpg',
     Description : '???'},
-    Aya2: { name: 'Aya', picture: 'assets/images/aya2.jpg' , Description : '???'},
+    Aya2: { name: 'Aya', picture: 'assets/images/aya2.jpg' , Description : 'I love weightlifting'},
     MahmmoudGamal: { name: 'Mahmmoud Gamal', picture: 'assets/images/MahmoudGamal.jpg'
     , Description : 'I love chess.'},
     Habiba: { name: 'Habiba', picture: 'assets/images/HabibaElHussein.jpg'
@@ -23,7 +33,7 @@ export class UserService {
     Description : 'I love Debugging' },
     Morgan: { name: 'Hesham Morgan', picture: 'assets/images/morgan.jpg',
     Description : 'I love Tech' },
-    Nada: { name: 'Nada', picture: 'assets/images/nada.jpg', Description : '???'},
+    Nada: { name: 'Nada', picture: 'assets/images/nada.jpg', Description : 'I love reading '},
     Sarah :{ name:'Sara Gamal', picture:'assets/images/SarahGamal.jpg',
     Description:'Languages, impressionism and symbols.'},
     Tarek :{ name:'Tarek Abdelrahman', picture:'assets/images/Tarek.JPG',
