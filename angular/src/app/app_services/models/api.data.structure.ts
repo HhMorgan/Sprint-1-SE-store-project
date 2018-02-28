0e6262d3c2d4c6ce04fc808b9bb64dc72a42697a
HhMorgan <heshammmse@gmail.com>
2018-02-28 12:13:59 -0800

    Finished my individual part

--- angular/src/app/app_services/models/api.data.structure.ts
+++ angular/src/app/app_services/models/api.data.structure.ts
@@ -8,3 +8,11 @@ export interface LoginData {
     username: String;
     password: String;
 }
+export interface ProductData {
+    _id:String;
+    name: String;
+    price: Number; 
+    createdAt: String; 
+    updatedAt: String;
+    seller:  String;
+}
