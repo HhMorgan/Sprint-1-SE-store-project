3be7ed585c43f633844874ec614aee6057c75884
omarhany1 <omar_hany97@hotmail.com>
2018-03-01 18:59:03 +0200

    AddToCart done in Backend

--- backend-nodejs/api/routes/index.js
+++ backend-nodejs/api/routes/index.js
@@ -1,14 +1,15 @@
 var express = require('express'),
-  router = express.Router(),
-  ProductController = require('../controllers/ProductController'),
-  UserController = require('../controllers/UserController');
-  CartController = require('../controllers/CartController');
+router = express.Router(),
+ProductController = require('../controllers/ProductController'),
+UserController = require('../controllers/UserController');
+
 
 //-------------------------------Product Routes-----------------------------------
 router.get('/product/getProducts', ProductController.getProducts);
 router.get('/product/getProduct/:productId', ProductController.getProduct);
-router.get('/product/getProductsBelowPrice/:price',
-  ProductController.getProductsBelowPrice
+router.get(
+'/product/getProductsBelowPrice/:price',
+ProductController.getProductsBelowPrice
 );
 router.post('/product/createProduct', ProductController.createProduct);
 router.post('/auth/login', UserController.userlogin);
@@ -17,12 +18,6 @@ router.post('/user/create', UserController.userCreate);
 
 router.patch('/product/updateProduct/:productId', ProductController.updateProduct);
 router.delete('/product/deleteProduct/:productId', ProductController.deleteProduct);
-//=============================Cart Routes==========================================
-router.get('/cart/getProducts', CartController.getProducts);
-router.get('/cart/getProduct/:productId', CartController.getProduct);
-router.get('/cart/getProductsBelowPrice/:price',
-CartController.getProductsBelowPrice
-);
+router.post('/user/addToCart', UserController.addToCart)
 
 module.exports = router;
-
