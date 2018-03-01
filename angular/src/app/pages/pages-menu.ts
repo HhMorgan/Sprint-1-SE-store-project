e6e2cb5d5e1b1a9ceb2f3668cbdd8145c9f5461d
ghostv247 <34899032+ghostv247@users.noreply.github.com>
2018-03-01 06:25:04 +0200

    added update function and toast messages

--- angular/src/app/pages/pages-menu.ts
+++ angular/src/app/pages/pages-menu.ts
@@ -6,18 +6,14 @@ export const MENU_ITEMS: NbMenuItem[] = [
     icon: 'nb-home',
     link: '/pages/dashboard',
     home: true,
-  },
-  {
+  },{
     title: 'Store',
     icon: 'nb-tables',
     link: '/pages/store',
-    home: true,
-  },
-  {
+  },{
     title: 'FEATURES',
     group: true,
-  },
-  {
+  },{
     title: 'UI Features',
     icon: 'nb-keypad',
     link: '/pages/ui-features',
@@ -25,32 +21,25 @@ export const MENU_ITEMS: NbMenuItem[] = [
       {
         title: 'Buttons',
         link: '/pages/ui-features/buttons',
-      },
-      {
+      },{
         title: 'Grid',
         link: '/pages/ui-features/grid',
-      },
-      {
+      },{
         title: 'Icons',
         link: '/pages/ui-features/icons',
-      },
-      {
+      },{
         title: 'Modals',
         link: '/pages/ui-features/modals',
-      },
-      {
+      },{
         title: 'Popovers',
         link: '/pages/ui-features/popovers',
-      },
-      {
+      },{
         title: 'Typography',
         link: '/pages/ui-features/typography',
-      },
-      {
+      },{
         title: 'Animated Searches',
         link: '/pages/ui-features/search-fields',
-      },
-      {
+      },{
         title: 'Tabs',
         link: '/pages/ui-features/tabs',
       },
@@ -63,8 +52,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
       {
         title: 'Form Inputs',
         link: '/pages/forms/inputs',
-      },
-      {
+      },{
         title: 'Form Layouts',
         link: '/pages/forms/layouts',
       },
@@ -77,7 +65,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
       {
         title: 'Tree',
         link: '/pages/components/tree',
-      }, {
+      },{
         title: 'Notifications',
         link: '/pages/components/notifications',
       },
@@ -90,16 +78,13 @@ export const MENU_ITEMS: NbMenuItem[] = [
       {
         title: 'Google Maps',
         link: '/pages/maps/gmaps',
-      },
-      {
+      },{
         title: 'Leaflet Maps',
         link: '/pages/maps/leaflet',
-      },
-      {
+      },{
         title: 'Bubble Maps',
         link: '/pages/maps/bubble',
-      },
-      {
+      },{
         title: 'Search Maps',
         link: '/pages/maps/searchmap',
       },
@@ -112,12 +97,10 @@ export const MENU_ITEMS: NbMenuItem[] = [
       {
         title: 'Echarts',
         link: '/pages/charts/echarts',
-      },
-      {
+      },{
         title: 'Charts.js',
         link: '/pages/charts/chartjs',
-      },
-      {
+      },{
         title: 'D3',
         link: '/pages/charts/d3',
       },
@@ -130,8 +113,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
       {
         title: 'TinyMCE',
         link: '/pages/editors/tinymce',
-      },
-      {
+      },{
         title: 'CKEditor',
         link: '/pages/editors/ckeditor',
       },
@@ -154,8 +136,7 @@ export const MENU_ITEMS: NbMenuItem[] = [
       {
         title: 'Login',
         link: '/pages/login',
-      },
-      {
+      },{
         title: 'Register',
         link: '/pages/signup',
       },
