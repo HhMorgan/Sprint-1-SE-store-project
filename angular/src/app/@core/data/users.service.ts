411849ccbe06126a28c6fba60add829a84d901de
Nada Hammouda <nadahammouda97@gmail.com>
2018-03-01 14:18:24 +0200

    Merging and solving conflicts.

--- angular/src/app/@core/data/users.service.ts
+++ angular/src/app/@core/data/users.service.ts
@@ -8,13 +8,7 @@ let counter = 0;
 export class UserService {
 
   private users = {
-<<<<<<< HEAD
-    AhmedAshraf: { name: 'Ahmed Ashraf', picture: 'assets/images/ahmedashraf.jpg'
-    , Description : '???'},
-    AhmedAyman: { name: 'Ahmed Ayman', picture: 'assets/images/Ahmedayman.jpg',
-    Description : '???'},
-    Aya2: { name: 'Aya', picture: 'assets/images/aya2.jpg' , Description : '???'},
-=======
+
     OmarHany: { name: 'Omar Hany', picture: 'assets/images/OmarHany.jpg'
     , Description : 'I love crossfit'},
     NourNounou: { name: 'Nour Nounou', picture: 'assets/images/NourNounou.png'
@@ -32,7 +26,7 @@ export class UserService {
     AhmedAyman: { name: 'Ahmed Ayman', picture: 'assets/images/Ahmedayman.jpg',
     Description : 'I love gaming'},
     Aya2: { name: 'Aya', picture: 'assets/images/aya2.jpg' , Description : 'I love weight lifting'},
->>>>>>> dev
+
     MahmmoudGamal: { name: 'Mahmmoud Gamal', picture: 'assets/images/MahmoudGamal.jpg'
     , Description : 'I love chess.'},
     Habiba: { name: 'Habiba', picture: 'assets/images/HabibaElHussein.jpg'
@@ -43,25 +37,19 @@ export class UserService {
     Description : 'I love Debugging' },
     Morgan: { name: 'Hesham Morgan', picture: 'assets/images/morgan.jpg',
     Description : 'I love Tech' },
-<<<<<<< HEAD
-    Nada: { name: 'Nada', picture: 'assets/images/nada.jpg', Description : '???'},
-=======
+
     Nada: { name: 'Nada', picture: 'assets/images/nada.jpg', Description : 'I love reading'},
->>>>>>> dev
+
     Sarah :{ name:'Sara Gamal', picture:'assets/images/SarahGamal.jpg',
     Description:'Languages, impressionism and symbols.'},
     Tarek :{ name:'Tarek Abdelrahman', picture:'assets/images/Tarek.JPG',
     Description:'Football is my passion' },
     Youhanna :{ name:'Youhanna Adel',picture:'assets/images/youhannaadel.jpg',
-<<<<<<< HEAD
-    Description:'???'},
-    Mariam :{ name:'Mariam Salama', picture:'assets/images/Mariam.JPG', Description:'I like to read'},
-    Salma :{ name:'Salma Zaki', picture:'assets/images/salmazaki.jpg', Description:'???'},
-=======
+
     Description:'I love basketball'},
     Mariam :{ name:'Mariam Salama', picture:'assets/images/Mariam.JPG', Description:'I like to read'},
     Salma :{ name:'Salma Zaki', picture:'assets/images/salmazaki.jpg', Description:'I love traveling'},
->>>>>>> dev
+
     Shaker :{ name:'Omar Shaker', picture:'assets/images/OmarShaker.jpg', Description:'I love coding.'},
     Hesham :{ name:'Mohamed Hesham', picture:'assets/images/mohamed_hesham.jpg', Description:'I love drawing and art in general'},
 
