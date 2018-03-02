1abb04487f31c1cd781d0a16149f12f9fc5551b5
ghostv247 <34899032+ghostv247@users.noreply.github.com>
2018-03-02 13:18:46 +0200

    master Merged With CartBackEnd

--- backend-nodejs/api/routes/index.js
+++ backend-nodejs/api/routes/index.js
@@ -5,24 +5,22 @@ UserController = require('../controllers/UserController');
 
 
 //-------------------------------Product Routes-----------------------------------
-router.get('/product/getProducts', ProductController.getProducts);
-router.get('/product/getProduct/:productId', ProductController.getProduct);
-router.get(
-'/product/getProductsBelowPrice/:price',
-ProductController.getProductsBelowPrice
-);
+router.get('/product/getProducts' , ProductController.getProducts);
+router.get('/product/getProduct/:productId' , ProductController.getProduct);
+router.get('/product/getProductsBelowPrice/:price' , ProductController.getProductsBelowPrice);
+
 router.post('/product/createProduct', ProductController.createProduct);
-router.post('/auth/login', UserController.userlogin);
-router.post('/user/create', UserController.userCreate);
+router.patch('/product/updateProduct/:productId' , ProductController.updateProduct);
+router.delete('/product/deleteProduct/:productId' , ProductController.deleteProduct);
 
+//-------------------------------User Routes-----------------------------------
+router.post('/auth/login' , UserController.userlogin);
+router.post('/user/create' , UserController.userCreate);
 
-router.patch('/product/updateProduct/:productId', ProductController.updateProduct);
-router.delete('/product/deleteProduct/:productId', ProductController.deleteProduct);
-router.post('/user/addToCart', UserController.addToCart);
-router.get('/user/viewCart', UserController.viewCart);
-router.get('/user/checkout', UserController.checkout);
+//-------------------------------User Cart Routes-----------------------------------
+router.post('/user/cart/checkout' , UserController.checkout);
+router.post('/user/cart/addToCart' , UserController.addToCart);
 
-
-
+router.get('/user/cart/viewCart/:username' , UserController.viewCart);
 
 module.exports = router;
